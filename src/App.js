e0e6a7b83ec89f6352527e9ca9202c630246a636
Bruno William <blin125@aucklanduni.ac.nz>
2023-12-25 15:46:41 +1300

    blank

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/blin125.github.io" element={<Home/>}/>
       </Routes>
     </Router>
   );
